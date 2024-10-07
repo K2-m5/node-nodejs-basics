28f27410ac5acd3f15c10ab16bf7892a2cc7b09e
Pavel Svirid <pavel.svirid@dfmx.com>
2024-10-07 23:01:16 +0300

    nodejs-basic cli, modules added

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const result = args.reduce((acc, current, index, array) => {
+        if (current.startsWith('--') && index + 1 < array.length) {
+            const propName = current.slice(2);
+            const propValue = array[index + 1];
+            acc.push(`${propName} is ${propValue}`);
+        }
+        
+        return acc;
+    }, []);
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
