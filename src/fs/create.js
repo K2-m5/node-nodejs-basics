055d08e75b2749879fba4c2f4d8c46e9ea8c1b41
Pavel Svirid <pavel.svirid@dfmx.com>
2024-10-07 14:20:11 +0300

    nodejs-basics create js added

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..');
+
 const create = async () => {
-    console.log('hi');
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+    const fileContent = 'I am fresh and young';
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.writeFile(filePath, fileContent);
+            console.log('File created successfully.');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await create();
